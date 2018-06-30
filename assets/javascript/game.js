d7063318895effe319adc32e6f6c6c813a3cdb0a
rgaritta <fender8@bellsouth.net>
2018-06-30 10:56:52 -0400

    firebase added

---
+++ assets/javascript/game.js
@@ -0,0 +1,28 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDHq7Nhsdz2v0GjUgNarEnEi_-lnV5QpvQ",
+    authDomain: "art-roulette-2ffee.firebaseapp.com",
+    databaseURL: "https://art-roulette-2ffee.firebaseio.com",
+    projectId: "art-roulette-2ffee",
+    storageBucket: "",
+    messagingSenderId: "1037636482050"
+};
+
+firebase.initializeApp(config);
+var database = firebase.database();
+
+database.ref().on("value", function (snapshot) {
+
+    console.log(snapshot.val().nouns);
+    console.log(snapshot.val().adjectives);
+
+}, function (errorObject) {
+    // Create Error Handling
+    console.log("The read failed: " + errorObject.code);
+});
+
+
+
+
+
+
