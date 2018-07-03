faa05ab73d8580ae893dc700113f893957a8008a
jon-castro <23022436+jon-castro@users.noreply.github.com>
2018-07-03 18:39:11 -0400

    Initial front-end work (Materialize)

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -62,9 +62,9 @@ function getAdj() {
 
 }
 
-getAdj();
-getNoun1();
-getNoun2();
+// getAdj();
+// getNoun1();
+// getNoun2();
 
 database.ref().on("value", function (snapshot) {
 
@@ -76,50 +76,65 @@ database.ref().on("value", function (snapshot) {
     console.log("The read failed: " + errorObject.code);
 });
 
-function UserLogin(userEmail,userPass)
-{
+function UserLogin(userEmail,userPass) {
 
 }
 
 
 $(window).on('load', function(){ 
-    $("#userLoginModal").show();
+    //$("#userLoginModal").show();
   });
 
-  $("#user-Error-Modal-Submit").on("click",function()
-{
-    $("#userLoginErrorModal").hide();
+  $("#user-Error-Modal-Submit").on("click",function() {
+  $("#userLoginErrorModal").hide();
 });
 
 //our modal dialog's submit button clicked
-$("#user-Login-Modal-Submit").on("click",function()
-{
+$("#user-Login-Modal-Submit").on("click",function() {
 
-//get the users email and password.
-var userEmail = $("#e-mail-Sub").val();
-var userPass = $("#pass-Sub").val();
+  //get the users email and password.
+  var userEmail = $("#e-mail-Sub").val();
+  var userPass = $("#pass-Sub").val();
 
-console.log("userEmail: " + userEmail);
-console.log("userPass: " + userPass);
+  console.log("userEmail: " + userEmail);
+  console.log("userPass: " + userPass);
 
-//we are going to hide our login modal just because if we are having issues we are going to need to display another modal.
-$("#userLoginModal").hide();
+  //we are going to hide our login modal just because if we are having issues we are going to need to display another modal.
+  $("#userLoginModal").hide();
 
-//if they failed to enter text for the email or password throw up an error modal
-if(userEmail === "" || userPass === "")
-{
+  //if they failed to enter text for the email or password throw up an error modal
+  if(userEmail === "" || userPass === "") {
     //fill the error title and text
     $("#user-Error-Modal-Title").text("User Error");
     $("#user-Error-Modal-Body").text("please provide both a User Name and a Password");
     $("#userLoginErrorModal").show();
-}
+  }
 
 
-// CHECK FOR INCOMPATIBLE LISTENERS!!!
-UserLogin(userEmail,userPass);
+  // CHECK FOR INCOMPATIBLE LISTENERS!!!
+  UserLogin(userEmail,userPass);
+});
+
+
 // Materialize JavaScript components
+
+
+
 $(document).ready(function() {
   // Navbar dropdown control - Materialize
-  $(".dropdown-trigger").dropdown();
-});
+  // $(".dropdown-trigger").dropdown();
+  // $(".fixed-action-btn").floatingActionButton();
+  $(".fixed-action-btn").floatingActionButton({
+    direction: "right"
+  });
+  $('.collapsible').collapsible();
+  $('.modal').modal();
+  $('.trigger-modal').modal();
+  $("#navbarButton").on("click", function() {
+    getAdj();
+    getNoun1();
+    getNoun2();
+  });
+
+
 });
